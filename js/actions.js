67896afa2eb1e7a1117f11bcd0c0a7479fad8685
nac1 <ignacio.h.velasco@gmail.com>
2015-01-31 10:18:06 -0600

    p11
    asd

---
+++ js/actions.js
@@ -0,0 +1,38 @@
+/*
+
+var fn = {
+    ready: function(){
+     //agregar al final 'deviceready'
+         alert('123');
+         document.addEventListener('deviceready',fn.init,false);
+        
+    },
+    init: function(){   
+        alert('1234');
+        $('#contact').tap(fn.agregar_contacto);
+        
+       // $('#regSend').click(fn.enviarRegistro);
+    //    $('#regFoto').click(capture.imgCapture);
+        
+        
+        
+        /*if(!storage.estaRegistrado())
+            window.location.href = '#reg';
+    },
+    agregar_contacto:function(){
+    contactos.creado();
+   }
+
+};
+
+$(fn.ready);
+*/
+ function init() {
+   document.addEventListener("deviceready", onDeviceReady, false);
+ }
+
+ function onDeviceReady() {
+   alert('It works!');
+ }
+
+
